6adc85998b8c8eda075a1200af6a2e5c66aeadcb
Baltasar Klix Jones <balta98_@hotmail.com>
2023-07-05 11:42:51 -0300

    validate models /POST && DELETE

--- database/src/middlewares/validateModel.js
+++ database/src/middlewares/validateModel.js
@@ -2,27 +2,30 @@ const { ModelError, PostError, DeleteError } = require("../../utils/errors");
 
 module.exports = (req, res, next) => {
   const validModels = ["Character", "Film", "Planet"];
-
   const { method } = req;
+  const { model } = req.params;
+  const { id } = req.params;
+
   if (method === "GET") {
-    const { model } = req.params;
     if (validModels.includes(model)) {
       return next();
     } else {
       throw new ModelError("Invalid model", 401);
     }
   } else if (method === "POST") {
-    const { _id, name } = req.body;
-    if (!_id || !name) {
-      throw new PostError("Invalid data", 401);
+    const {_id, name, title } = req.body;
+    if (!_id) {
+      throw new PostError("Id is necesary", 401);
+    }
+    if (model === validModels[0] && !name) {
+      throw new PostError("Name is necesary", 401);
+    }
+    if (model === validModels[1] && !title) {
+      throw new PostError("Title is necesary", 401);
     } else {
       return next();
     }
   } else if (method === "DELETE") {
-    const { id, model } = req.params;
-
-    const validModels = ["Character", "Film", "Planet"];
-
     if (!id) {
       throw new DeleteError("Invalid ID", 401);
     } else if (!validModels.includes(model)) {
